a460eb078b73e53e77d5d738142deaa841a70b6e
Tolulope <tolulopebamisile@gmail.com>
2024-07-11 16:22:56 +0100

    new

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,13 +1,17 @@
+import { IsNumber, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Category {
   @PrimaryGeneratedColumn()
+  @IsNumber()
   id: number;
 
   @Column()
+  @IsString()
   name: string;
 
   @Column()
+  @IsString()
   color: string;
 }
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,9 +1,11 @@
 import { v4 } from 'uuid';
 import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDate, IsJSON, IsNumber, IsString, IsUUID } from 'class-validator';
 
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
   id: string;
 
   @BeforeInsert()
@@ -12,17 +14,22 @@ export class Task {
   }
 
   @Column()
+  @IsString()
   todo: string;
 
   @Column()
+  @IsString()
   description: string;
 
   @Column()
+  @IsDate()
   date: Date;
 
   @Column()
+  @IsNumber()
   priority: number;
 
   @Column('jsonb')
+  @IsJSON()
   tags: { name: string; color: string };
 }
