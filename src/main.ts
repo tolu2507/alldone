993b13542f43af506d007d816d2049b0a06d23e3
Tolulope Bamisile <109019116+tolu2507@users.noreply.github.com>
2024-08-01 23:41:22 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8081', // Specify the origin or use '*' to allow all origins
+    origin: '*',    
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
